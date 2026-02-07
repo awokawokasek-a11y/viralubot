553e9a829eedd8a23f23ff6886841d98cc58e84a
awokawokasek-a11y <awokawokasek@gmail.com>
2026-02-07 16:43:22 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH", "")
+SESSION_STRING = os.getenv("SESSION_STRING", "")
+OWNER_ID = int(os.getenv("OWNER_ID", 0))
+
+PREFIX = os.getenv("PREFIX", ".")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
